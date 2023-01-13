1fdbaf9046ec7480ea89e1bcd569f2a6f8cdd244
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-13 13:19:14 +0530

    linked list create and print

--- src/main/java/com/bootcoding/dsa/LinkedList/Implement.java
+++ src/main/java/com/bootcoding/dsa/LinkedList/Implement.java
@@ -34,6 +34,46 @@ public class Implement {
             temp = temp.next;
         }
     }
+    //Inserting first node in list
+    public static Implement firstInsert(Implement head)
+    {
+        Implement imp = new Implement(70);
+        imp.next=head;
+        head=imp;
+        return head;
+    }
+    //Inserting last node in list
+    public static void lastInsertNode(Implement head)
+    {
+        Implement temp = head;
+        Implement imp = new Implement(50);
+        System.out.println(head);
+        while (temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = imp;
+    }
+    //Deleting first node in list
+    public static void deleteFirstNode(Implement head)
+    {
+        head = head.next;
+        print(head);
+    }
+    //deleting last node in list
+    public static void deleteLastNode(Implement head)
+    {
+        Implement temp = head;
+        while (temp != null)
+        {
+            System.out.println(temp.data);
+            if (temp.next.next == null)
+            {
+                temp.next = null;
+            }
+            temp = temp.next;
+        }
+    }
 
     public static void main(String[] args) {
 
