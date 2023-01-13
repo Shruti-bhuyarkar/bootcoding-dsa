81f400aa7d9e1b74616c9b524ad6abb67542b5ec
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-13 13:18:36 +0530

    Asterisk pattern

---
+++ src/main/java/com/bootcoding/dsa/Pattern/Asterisk.java
@@ -0,0 +1,15 @@
+package com.bootcoding.dsa.Pattern;
+
+public class Asterisk {
+    public static void main(String[] args) {
+       int i,j,row=6;
+       for ( i = 0; i<row; i++)
+       {
+           for ( j = 0; j<=i; j++)
+           {
+               System.out.print("* ");
+           }
+           System.out.println();
+       }
+    }
+}
