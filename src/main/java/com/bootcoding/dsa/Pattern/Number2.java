7a42b1a4246be941e546d8f7e6d96fcf22d1c93a
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-13 13:19:56 +0530

    number pattern

---
+++ src/main/java/com/bootcoding/dsa/Pattern/Number2.java
@@ -0,0 +1,15 @@
+package com.bootcoding.dsa.Pattern;
+
+public class Number2 {
+    public static void main(String[] args) {
+        int i,j,n=6;
+        for ( i = 1; i<=n; i++)
+        {
+            for ( j = 1; j<=i; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
