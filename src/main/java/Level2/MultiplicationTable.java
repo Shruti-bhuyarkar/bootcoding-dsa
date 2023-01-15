30465eaedfc4e2e93becf1b1ae9df11e6879774f
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-15 23:32:39 +0530

    Multiplication Table

---
+++ src/main/java/Level2/MultiplicationTable.java
@@ -0,0 +1,24 @@
+package Level2;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int number = sc.nextInt();
+        System.out.println("Multiplication Table ! ");
+        findMultiplicationTable(number);
+
+        }
+        public static void findMultiplicationTable(int number){
+        int num = number;
+        int Mul = 0;
+        for (int i = 1; i<=10; i++){
+            Mul = num * i;
+
+            System.out.println(num + " X " + i + " = " + Mul );
+        }
+        }
+    }
+
