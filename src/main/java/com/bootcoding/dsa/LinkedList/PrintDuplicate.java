aaa9321a622730f1266fef7e7e2cb99b71fe61c5
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-17 13:48:06 +0530

    .

--- src/main/java/com/bootcoding/dsa/LinkedList/PrintDuplicate.java
+++ src/main/java/com/bootcoding/dsa/LinkedList/PrintDuplicate.java
@@ -3,7 +3,7 @@ package com.bootcoding.dsa.LinkedList;
 public class PrintDuplicate {
     public static void main(String[] args) {
         Node firstNode = new Node(1);
-        Node secondNode = new Node(2);
+        Node secondNode = new Node(1);
         Node thirdNode = new Node(3);
         Node fourthNode = new Node(4);
         Node fifthNode = new Node(3);
@@ -19,19 +19,16 @@ public class PrintDuplicate {
     }
     public static void printDuplicate(Node head){
         Node temp = head;
-        int DuplicateCounter = 0;
         while (temp.next != null) {
             Node temp2 = temp.next;
             while(temp2 != null) {
                 if (temp.data == temp2.data) {
                     System.out.println(temp.data);
-                    return;
+                    
                 }
                 temp2 = temp2.next;
-            }
-
+            } temp2 = head;
             temp = temp.next;
         }
-
     }
 }
