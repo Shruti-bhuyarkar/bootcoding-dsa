484e62d7bb5e979e0708c98502b0291511f8e188
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-18 00:40:36 +0530

    Count Number of Digits

---
+++ src/main/java/com/bootcoding/dsa/Recursion/CountDigit.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.Recursion;
+
+public class CountDigit {
+    public static void main(String[] args) {
+        int num = 1003;
+        System.out.println("Count of digits : " + getcountDigit(num));
+    }
+
+    public static int getcountDigit(int num){
+        {
+            if (num/10 == 0)
+                return 1;
+            return 1 + getcountDigit(num / 10);
+        }
+    }
+}
