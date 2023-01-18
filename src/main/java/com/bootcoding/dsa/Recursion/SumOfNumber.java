826677a0fc3c3ea3de40977c6c481d4d3aad7c45
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-19 00:04:54 +0530

    .

--- src/main/java/com/bootcoding/dsa/Recursion/SumOfNumber.java
+++ src/main/java/com/bootcoding/dsa/Recursion/SumOfNumber.java
@@ -2,14 +2,13 @@ package com.bootcoding.dsa.Recursion;
 
 public class SumOfNumber {
     public static void main(String[] args) {
-        int n = 10 ;
-        System.out.println(printNaturalNumbers(n));
-    }
+        int n = 99 ;
+        System.out.println(printNaturalNumbers(n));}
     public static int printNaturalNumbers(int n ){
-        if(n<=1) {
+        if(n < 10) {
             return n;
         }
-        return n + printNaturalNumbers(n-1);
+        return n % 10 + printNaturalNumbers(n / 10);
     }
 }
 
