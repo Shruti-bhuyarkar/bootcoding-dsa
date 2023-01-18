c172978266d0125e204a45fc7776f543317e5ba3
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-19 00:03:24 +0530

    Array Element in Recursion

---
+++ src/main/java/com/bootcoding/dsa/Recursion/ArrayElement.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.Recursion;
+
+public class ArrayElement {
+    public static void main(String[] args) {
+        int[] n = {10,20,30,40,50,60};
+        printArrayElement(n , 0);
+    }
+    public static void printArrayElement(int[] n, int a){
+        if(a == n.length){
+            return;
+        }
+        System.out.println(n[a]);
+        printArrayElement(n , ++a);
+    }
+
+}
