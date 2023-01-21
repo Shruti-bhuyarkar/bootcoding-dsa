c6a8cb11600c7607659f235aa6dff836bf9280f8
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-21 19:30:54 +0530

    Count Digits using Array

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/ContainDuplicate.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa.LeetCode;
+
+public class ContainDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(printcontainsDuplicate(nums));
+    }
+        public static boolean printcontainsDuplicate(int[] nums) {
+            for (int i = 0; i<nums.length; i++){
+                for (int j = (i+1); j<nums.length; j++){
+                    if (nums[i]==nums[j]){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+
