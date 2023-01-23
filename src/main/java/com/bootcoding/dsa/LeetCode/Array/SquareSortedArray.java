3c430543e4c7b10b35b6e2dded52ee2961020ac9
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-24 00:39:28 +0530

    Square Sorted Array in Array LeetCode Num 977

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/Array/SquareSortedArray.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa.LeetCode.Array;
+
+import java.util.Arrays;
+
+public class SquareSortedArray {
+    public static void main(String[] args) {
+    int[] nums = {-4, -1, 0, 3, 10};
+    int[] square = sortedSquares(nums);
+    for (int i = 0; i < square.length; i++) {
+        System.out.println(square[i]);
+    }
+    }
+    public static int[] sortedSquares(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
