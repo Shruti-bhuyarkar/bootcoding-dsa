4114c10778b91dc52ac666e98a7f6e04306bf5ff
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-24 00:41:20 +0530

    find Target Indices with even number of digits in Array LeetCode Num 2089

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/Array/TargetIndices.java
@@ -0,0 +1,27 @@
+package com.bootcoding.dsa.LeetCode.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TargetIndices {
+    public static void main(String[] args) {
+    int[] nums = {1, 2, 5, 2, 3};
+    //int target = 0;
+    List<Integer> TI = targetIndices(nums, 2);
+    for (int i = 0; i< TI.size(); i++){
+        System.out.println(TI.get(i));
+    }
+    }
+    public static  List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> index = new ArrayList<>();
+        for (int i = 0 ; i < nums.length ; i++){
+
+            if(nums[i] == target){
+                index.add(i);
+            }
+        }
+        return index;
+    }
+}
