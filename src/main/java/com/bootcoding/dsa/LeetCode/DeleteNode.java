4946d90b667b3597ab70effa5c001d0ead8433ee
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-23 10:24:31 +0530

    Delete Node in a Linked List  LeetCode 237

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/DeleteNode.java
@@ -0,0 +1,40 @@
+package com.bootcoding.dsa.LeetCode;
+
+import com.bootcoding.dsa.LinkedList.ListNode;
+
+public class DeleteNode {
+    public static void main(String[] args) {
+        ListNode firstNode = new ListNode(4);
+        ListNode secondNode = new ListNode(5);
+        ListNode thirdNode = new ListNode(1);
+        ListNode fourthNode = new ListNode(9);
+        // ListNode fifthNode = new ListNode(5);
+
+        ListNode head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = fourthNode;
+        fourthNode.next = null;
+
+        //print(head);
+      deleteNode(secondNode);
+       print(head);
+    }
+
+    public static ListNode deleteNode(ListNode node) {
+
+           node.val = node.next.val;
+            node.next = node.next.next;
+          // System.out.println(node);
+        return node;
+    }
+        public static void print(ListNode head){
+            ListNode temp = head;
+            while (temp != null){
+                System.out.println(temp.val);
+                temp = temp.next;
+            }
+        }
+
+    }
+
