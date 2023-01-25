1a0e8499cb4285f78a8ef7cb5c49cf57a9750f84
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-25 15:06:03 +0530

    .

--- src/main/java/com/bootcoding/dsa/hashtable/HashTableExample.java
+++ src/main/java/com/bootcoding/dsa/hashtable/HashTableExample.java
@@ -7,19 +7,20 @@ public class HashTableExample {
         int[] a = {10, 20, 30, 40, 20, 10};
         //find duplicate element in an array
 
-        HashMap store = new HashMap();
+        HashMap<Integer , Integer> store = new HashMap<>();
         for (int i = 0; i < a.length; i++){
            // Object store;
             if(store.containsKey(a[i])){
-               int val = (int) store.get(a[i]);
+               int val =  store.get(a[i]);
                store.put(a[i], val + 1);
             }else {
                 store.put(a[i] , 1);
             }
         }
-        for (Object key : store.keySet()){
-            int k = (int) key;
-            int val = (int) store.get(k);
+
+        for (Integer key : store.keySet()){
+            int k = key;
+            int val = store.get(k);
             if(val > 1){
                 System.out.println(k);
             }
