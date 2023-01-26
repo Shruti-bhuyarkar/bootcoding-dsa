6ab7cab73234c5ba0d4794f3201f1951fff26f4b
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-26 10:33:27 +0530

    Tribonacci Number using Hash table

---
+++ src/main/java/com/bootcoding/dsa/hashtable/TribonacciNumber.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa.hashtable;
+
+import java.util.HashMap;
+
+public class TribonacciNumber {
+    public static void main(String[] args) {
+        System.out.println(tribonacci(4));
+    }
+    static HashMap<Integer, Integer> index = new HashMap<>();
+    public static int tribonacci(int n) {
+        if(n == 0)
+            return 0;
+        if(n == 1 || n == 2)
+            return 1;
+        if (index.containsKey(n))
+            return index.get(n);
+        index.put(n, tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3));
+            return index.get(n);
+        }
+    }
+
