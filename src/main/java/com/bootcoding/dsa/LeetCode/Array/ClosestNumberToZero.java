d3bfb6db89b0f0e74c7566a309dabeb37927f60c
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-01-27 09:37:50 +0530

    Closest Number to zero

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/Array/ClosestNumberToZero.java
@@ -0,0 +1,19 @@
+package com.bootcoding.dsa.LeetCode.Array;
+
+public class ClosestNumberToZero {
+    public static void main(String[] args) {
+    int[] nums = {-4, -2, 1, 4, 8};
+        System.out.println(findClosestNumber(nums));
+    }
+    public static int findClosestNumber(int[] nums) {
+        int[] n = nums;
+        //maths.abs() it's a method who returns the absolute(positive) value of number
+        int count = Math.abs(n[n.length - 1]);
+        for (int i = 0; i < n.length; i++){
+            int j = Math.abs(n[i]);
+            if (j < count)
+                count = j;
+        }
+        return count;
+    }
+}
