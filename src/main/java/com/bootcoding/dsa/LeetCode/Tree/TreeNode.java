2f6d19a1178102458327f0ae2420c00ff2996d05
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-02-01 18:14:39 +0530

    Tree Node

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/Tree/TreeNode.java
@@ -0,0 +1,13 @@
+package com.bootcoding.dsa.LeetCode.Tree;
+
+public class TreeNode {
+    int data ;
+    TreeNode left;
+    TreeNode right;
+
+   public TreeNode(int data){
+       this.left = left;
+       this.right = right;
+       this.data = data;
+   }
+}
