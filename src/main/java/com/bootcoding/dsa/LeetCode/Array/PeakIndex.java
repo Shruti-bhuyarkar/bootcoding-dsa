a33599f0aa513dbcee8775bb060cba229febe182
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-02-02 09:43:06 +0530

    Peak Index LeetCode 852

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/Array/PeakIndex.java
@@ -0,0 +1,23 @@
+package com.bootcoding.dsa.LeetCode.Array;
+
+public class PeakIndex {
+    public static void main(String[] args) {
+        //int[] arr = {0, 2, 1, 0};
+        int[] arr = {0, 10, 5, 2};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+    public static  int peakIndexInMountainArray(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        while(low < high){
+            //int mid = (low + high)/2 -- overflow the integer
+            int mid = low + (high - low) / 2;
+            if(arr[mid] < arr[mid + 1]){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
