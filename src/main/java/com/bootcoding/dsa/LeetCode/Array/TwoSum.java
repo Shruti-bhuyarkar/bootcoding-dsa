df642e7be0fdc49719cdcb3f70c3ba802bc72a0a
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-02-03 09:32:59 +0530

    Two Sum LeetCode num 1

--- src/main/java/com/bootcoding/dsa/LeetCode/Array/TwoSum.java
+++ src/main/java/com/bootcoding/dsa/LeetCode/Array/TwoSum.java
@@ -7,10 +7,13 @@ public class TwoSum {
     static HashMap<Integer, Integer> Index = new HashMap<>();
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
-        System.out.println(twoSum(nums, 9));
+      int[] ans =  twoSum(nums, 9);
+       for(int i = 0; i < ans.length; i++){
+           System.out.println(ans[i]);
+       }
+
     }
     public static int[] twoSum(int[] nums, int target) {
-
         for (int i = 0; i < nums.length; i++) {
             if (Index.containsKey(target - nums[i])) {
                 return new int[] {Index.get(target - nums[i]), i};
