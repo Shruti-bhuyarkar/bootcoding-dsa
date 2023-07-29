304fa12df37a3bdf8573998ff879b5d2b81311c1
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-07-30 00:46:42 +0530

    height of the tree

--- src/main/java/com/bootcoding/dsa/LeetCode/Tree/HeightOfTree.java
+++ src/main/java/com/bootcoding/dsa/LeetCode/Tree/HeightOfTree.java
@@ -7,15 +7,24 @@ public class HeightOfTree {
         treeNode n3 = new treeNode(3);
         treeNode n4 = new treeNode(4);
         treeNode n5 = new treeNode(5);
+        treeNode n6 = new treeNode(6);
+        treeNode n7 = new treeNode(7);
 
         root.left = n2;
         root.right = n3;
         n2.right = n5;
         n2.left = n4;
+        n3.left = n6;
+        n4.right = n7;
 
-        findHeight(root);
+        System.out.println(findHeight(root));
     }
-    public static void findHeight(treeNode root){
-
+    public static int findHeight(treeNode root){
+        if (root == null) {
+            return 0;
+        }
+        int HT = findHeight(root.left);
+        int RT = findHeight(root.right);
+        return 1+Math.max(HT, RT);
     }
 }
