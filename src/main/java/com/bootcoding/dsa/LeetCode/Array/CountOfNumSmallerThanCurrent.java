24b7d54f05f50cf8acb0540ddd4db30c54cda713
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-19 17:39:36 +0530

    Count Of Num Smaller Than Current

--- src/main/java/com/bootcoding/dsa/LeetCode/Array/CountOfNumSmallerThanCurrent.java
+++ src/main/java/com/bootcoding/dsa/LeetCode/Array/CountOfNumSmallerThanCurrent.java
@@ -1,12 +1,12 @@
 package com.bootcoding.dsa.LeetCode.Array;
 
 public class CountOfNumSmallerThanCurrent {
-    public static void main(String[] args) {
-    int[] nums = {8, 1, 2, 2, 3};
-    int[] SMTC = smallerNumbersThanCurrent(nums);
-    for (int i = 0; i<SMTC.length; i++){
-        System.out.println(SMTC[i]);
-    }
+        public static void main(String[] args) {
+        int[] nums = {8, 1, 2, 2, 3};
+        int[] SMTC = smallerNumbersThanCurrent(nums);
+        for (int i = 0; i<SMTC.length; i++){
+            System.out.println(SMTC[i]);
+        }
     }
     public static int[] smallerNumbersThanCurrent(int[] nums) {
         int[] nums2 = new int[nums.length];
