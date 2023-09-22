b0ab11770d9881b505014de6e220b54de02b5fe6
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-22 18:01:46 +0530

    sqrt

--- src/main/java/com/bootcoding/dsa/LeetCode/Array/sqrt.java
+++ src/main/java/com/bootcoding/dsa/LeetCode/Array/sqrt.java
@@ -1,18 +1,15 @@
 package com.bootcoding.dsa.LeetCode.Array;
 
 public class sqrt {
-
         public static void main(String[] args) {
             int x = 4; // The non-negative integer for which we want to find the square root
             int result = mySqrt(x);
             System.out.println("The square root of " + x + " is " + result);
         }
-
         public static int mySqrt(int x) {
             if (x == 0) {
                 return 0;
             }
-
             int left = 1, right = x;
             while (left <= right) {
                 int mid = left + (right - left) / 2;
