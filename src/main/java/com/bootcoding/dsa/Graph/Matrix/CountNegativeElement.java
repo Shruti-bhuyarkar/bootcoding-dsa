c6920d914c2717bb7ab7391499e8cbf4e96598ec
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-25 13:30:51 +0530

    count negative element

--- src/main/java/com/bootcoding/dsa/Graph/Matrix/CountNegativeElement.java
+++ src/main/java/com/bootcoding/dsa/Graph/Matrix/CountNegativeElement.java
@@ -8,7 +8,6 @@ public class CountNegativeElement {
                 {-1,  1,  2, 3},
                 { 1,  2,  4, 5}
         };
-
         int negativeCount = countNegatives(matrix);
         System.out.println("Number of negative numbers in the sorted matrix: " + negativeCount);
     }
@@ -28,7 +27,6 @@ public class CountNegativeElement {
 //        }
 //        return count;
         int count = 0;
-
         for (int[] row : grid) {
             for (int num : row) {
                 if (num < 0) {
@@ -36,7 +34,6 @@ public class CountNegativeElement {
                 }
             }
         }
-
         return count;
     }
 }
