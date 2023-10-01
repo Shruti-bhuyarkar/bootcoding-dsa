1bacb106ba30012019e2cb5e9fa42fa983e9d398
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-10-01 14:06:39 +0530

    reverse string

--- src/main/java/com/bootcoding/dsa/ReverseString.java
+++ src/main/java/com/bootcoding/dsa/ReverseString.java
@@ -1,9 +1,7 @@
 package com.bootcoding.dsa;
 
 public class ReverseString {
-    public static void main(String[] args) {
-        //reverseString();
-    }
+
     public static  void reverseString(char[] s) {
         for(int i=0 ;i<s.length/2;i++){
             char temp = s[i];
