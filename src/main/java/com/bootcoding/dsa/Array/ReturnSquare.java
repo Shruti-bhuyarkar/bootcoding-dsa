7c834e6073627198d5c37c537f5c05be9340becc
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-10-09 18:48:38 +0530

    Return Square

--- src/main/java/com/bootcoding/dsa/Array/ReturnSquare.java
+++ src/main/java/com/bootcoding/dsa/Array/ReturnSquare.java
@@ -2,7 +2,6 @@ package com.bootcoding.dsa.Array;
 // WAP to return square of array element using method
 public class ReturnSquare {
     public static void main(String[] args) {
-
         int[] num = {2, 3};
         int[] result =  ReturnSquare(num);
         for(int i = 0; i< result.length; i++){
@@ -12,7 +11,6 @@ public class ReturnSquare {
     public static int[] ReturnSquare ( int[] digits) {
             for (int i = 0; i < digits.length; i++) {
                 digits[i] = digits[i] * digits[i];
-
             }
             return digits;
         }
