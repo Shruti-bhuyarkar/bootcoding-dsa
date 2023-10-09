0b29d5eba21d36d84fafb83ffe0ac55666cb6b62
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-10-09 18:49:00 +0530

    Running Array

--- src/main/java/com/bootcoding/dsa/Array/RunningArray.java
+++ src/main/java/com/bootcoding/dsa/Array/RunningArray.java
@@ -13,7 +13,6 @@ public class RunningArray {
         int a[] = new int[nums.length];
         for(int i = 0; i<nums.length;i++){
             int sum = 0;
-
             for(int j = 0;j<=i;j++){
                 sum += nums[j];
             }
