9aeed02180794c02a6a6cd4c9eb2612d96bf8c62
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-11-04 10:39:49 +0530

    Find 132 pattern

--- src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Medium/Find132Pattern.java
+++ src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Medium/Find132Pattern.java
@@ -17,16 +17,13 @@ public class Find132Pattern {
             if (n < 3) {
                 return false;
             }
-
             int[] minArray = new int[n];
             minArray[0] = nums[0];
 
             for (int i = 1; i < n; i++) {
                 minArray[i] = Math.min(minArray[i - 1], nums[i]);
             }
-
             Stack<Integer> stack = new Stack<>();
-
             for (int j = n - 1; j >= 0; j--) {
                 if (nums[j] > minArray[j]) {
                     while (!stack.isEmpty() && stack.peek() <= minArray[j]) {
