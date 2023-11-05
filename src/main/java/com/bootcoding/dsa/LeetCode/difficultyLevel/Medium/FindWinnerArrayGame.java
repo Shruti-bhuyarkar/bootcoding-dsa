680a42c08d40d227f32369cbdc8bd6ad9e090335
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-11-05 14:33:24 +0530

    Find the Winner of an Array Game

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Medium/FindWinnerArrayGame.java
@@ -0,0 +1,43 @@
+package com.bootcoding.dsa.LeetCode.difficultyLevel.Medium;
+import java.util.Arrays;
+public class FindWinnerArrayGame {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 3, 5, 4, 6, 7};
+        int k = 2;
+
+        FindWinnerArrayGame findWinnerArrayGame = new FindWinnerArrayGame();
+        int winner = findWinnerArrayGame.getWinner(arr, k);
+        System.out.println("The winner is: " + winner);
+    }
+        public int getWinner(int[] arr, int k) {
+            if (k == 1) {
+                return Math.max(arr[0], arr[1]);
+            }
+            if (k >= arr.length) {
+                return Arrays.stream(arr).max().getAsInt();
+            }
+
+            int current_winner = arr[0];
+            int consecutive_wins = 0;
+
+            for (int i = 1; i < arr.length; i++) {
+                if (current_winner > arr[i]) {
+                    consecutive_wins++;
+                } else {
+                    current_winner = arr[i];
+                    consecutive_wins = 1;
+                }
+
+                if (consecutive_wins == k) {
+                    return current_winner;
+                }
+            }
+
+            return current_winner;
+        }
+
+
+    }
+
+
