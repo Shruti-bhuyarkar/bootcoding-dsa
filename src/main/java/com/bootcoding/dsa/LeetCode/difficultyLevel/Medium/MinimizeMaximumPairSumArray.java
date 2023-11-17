e3cae2a053bfb2e506dcdbee9a805677e0fe55dc
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-11-17 09:08:43 +0530

    Minimize Maximum Pair Sum in Array

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Medium/MinimizeMaximumPairSumArray.java
@@ -0,0 +1,42 @@
+package com.bootcoding.dsa.LeetCode.difficultyLevel.Medium;
+import java.util.Arrays;
+public class MinimizeMaximumPairSumArray {
+
+
+    public static void main(String[] args) {
+
+        MinimizeMaximumPairSumArray solution = new MinimizeMaximumPairSumArray();
+        int[] nums = {3, 5, 2, 6};
+        int result = solution.minPairSum(nums);
+        System.out.println("Minimum of the Maximum Pair Sum: " + result);
+    }
+        public int minPairSum(int[] nums) {
+            // Step 1: Sort the array in ascending order
+            Arrays.sort(nums);
+
+            // Step 2: Initialize pointers at the start and end of the sorted array
+            int left = 0, right = nums.length - 1;
+
+            // Step 3: Initialize a variable to store the minimum of the maximum pair sum
+            int minMaxPairSum = Integer.MIN_VALUE;
+
+            // Step 4: Iterate until the pointers meet
+            while (left < right) {
+                // Step 5: Calculate the current pair sum
+                int currentPairSum = nums[left] + nums[right];
+
+                // Step 6: Update the minimum of the maximum pair sum
+                minMaxPairSum = Math.max(minMaxPairSum, currentPairSum);
+
+                // Step 7: Move the pointers towards the center
+                left++;
+                right--;
+            }
+
+            // Step 8: Return the minimum of the maximum pair sum
+            return minMaxPairSum;
+        }
+
+    }
+
+
