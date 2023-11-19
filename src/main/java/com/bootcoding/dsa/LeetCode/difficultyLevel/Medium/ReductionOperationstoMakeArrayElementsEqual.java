7dc4c75d7cc3fff9a4f7460905d40b4ec7cdc958
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-11-19 07:51:25 +0530

    Reduction Operations to Make the Array Elements Equal

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Medium/ReductionOperationstoMakeArrayElementsEqual.java
@@ -0,0 +1,30 @@
+package com.bootcoding.dsa.LeetCode.difficultyLevel.Medium;
+
+public class ReductionOperationstoMakeArrayElementsEqual {
+
+    public static void main(String[] args) {
+
+        ReductionOperationstoMakeArrayElementsEqual R = new ReductionOperationstoMakeArrayElementsEqual();
+        int[] nums = {5, 1, 3};
+        int result = R.reductionOperations(nums);
+        System.out.println("Reduction Operations: " + result);
+    }
+        public int reductionOperations(int[] nums) {
+            int n = nums.length;
+            int[] freq = new int[50001];
+            for (int i = 0; i < n; i++) {
+                freq[nums[i]]++;
+            }
+            int res = 0, operations = 0;
+            for (int i = 50000; i >= 1; i--) {
+                if (freq[i] > 0) {
+                    operations += freq[i];
+                    res += operations - freq[i];
+                }
+            }
+            return res;
+        }
+
+    }
+
+
