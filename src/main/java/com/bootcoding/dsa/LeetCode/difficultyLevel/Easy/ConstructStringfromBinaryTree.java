d3f09400e9b174c8b8e53df9f868d6f72fef9049
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-12-08 10:08:14 +0530

    Construct String from Binary Tree

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Easy/ConstructStringfromBinaryTree.java
@@ -0,0 +1,54 @@
+package com.bootcoding.dsa.LeetCode.difficultyLevel.Easy;
+
+import com.bootcoding.dsa.LeetCode.Tree.treeNode;
+
+public class ConstructStringfromBinaryTree {
+
+    public static void main(String[] args) {
+        ConstructStringfromBinaryTree solution = new ConstructStringfromBinaryTree();
+
+        treeNode root = new treeNode(1);
+        root.left = new treeNode(2);
+        root.right = new treeNode(3);
+        root.left.left = new treeNode(4);
+
+        String result = solution.tree2str(root);
+
+        System.out.println("String representation of the binary tree: " + result);
+    }
+        public String tree2str(treeNode root) {
+            if (root == null) {
+                return "";
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            dfs(root, result);
+
+            return result.toString();
+        }
+
+        private void dfs(treeNode node, StringBuilder result) {
+            if (node == null) {
+                return;
+            }
+
+            result.append(node.data);
+
+            if (node.left != null || node.right != null) {
+                result.append("(");
+                dfs(node.left, result);
+                result.append(")");
+
+                if (node.right != null) {
+                    result.append("(");
+                    dfs(node.right, result);
+                    result.append(")");
+                }
+            }
+        }
+
+
+    }
+
+
