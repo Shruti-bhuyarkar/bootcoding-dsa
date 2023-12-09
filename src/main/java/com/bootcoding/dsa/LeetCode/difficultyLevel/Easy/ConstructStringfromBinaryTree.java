7a14707fa0bbda5bab93ae02691da318724fd229
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-12-09 20:36:03 +0530

    Construct String from Binary Tree

--- src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Easy/ConstructStringfromBinaryTree.java
+++ src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Easy/ConstructStringfromBinaryTree.java
@@ -3,7 +3,6 @@ package com.bootcoding.dsa.LeetCode.difficultyLevel.Easy;
 import com.bootcoding.dsa.LeetCode.Tree.treeNode;
 
 public class ConstructStringfromBinaryTree {
-
     public static void main(String[] args) {
         ConstructStringfromBinaryTree solution = new ConstructStringfromBinaryTree();
 
@@ -11,9 +10,7 @@ public class ConstructStringfromBinaryTree {
         root.left = new treeNode(2);
         root.right = new treeNode(3);
         root.left.left = new treeNode(4);
-
         String result = solution.tree2str(root);
-
         System.out.println("String representation of the binary tree: " + result);
     }
         public String tree2str(treeNode root) {
