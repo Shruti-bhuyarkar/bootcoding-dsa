088f1ed2f704e4ac91455a99e210efcc6a611a01
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-12-18 12:02:47 +0530

    Maximum Product Difference Between Two Pairs

---
+++ src/main/java/com/bootcoding/dsa/LeetCode/difficultyLevel/Easy/ProductDifference.java
@@ -0,0 +1,32 @@
+package com.bootcoding.dsa.LeetCode.difficultyLevel.Easy;
+// Maximum Product Difference Between Two Pairs
+import java.util.Arrays;
+
+public class ProductDifference {
+    public static void main(String[] args) {
+        int[] nums = {4, 2, 5, 9, 7, 4, 8};
+        int result = maxProductDifference(nums);
+        System.out.println("Maximum Product Difference: " + result);
+    }
+            public static int maxProductDifference(int[] nums) {
+            // Sort the array in ascending order
+            Arrays.sort(nums);
+
+            // Find the maximum and second maximum elements
+            int max1 = nums[nums.length - 1];
+            int max2 = nums[nums.length - 2];
+
+            // Find the minimum and second minimum elements
+            int min1 = nums[0];
+            int min2 = nums[1];
+
+            // Calculate the maximum product difference
+            int result = (max1 * max2) - (min1 * min2);
+
+            return result;
+        }
+
+
+    }
+
+
